be1b1830c92b590c6c0de92ebc4cf784df30a657
Piotr Rokita <rokitapiotr97@gmail.com>
2024-01-06 00:23:48 +0100

    update

--- file_utils/data_utils.py
+++ file_utils/data_utils.py
@@ -1,7 +1,7 @@
 import csv
 from pathlib import Path
 
-print("BASE_DIR:", Path(__file__).resolve().parent.parent)
+#print("BASE_DIR:", Path(__file__).resolve().parent.parent)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEST_DATA_DIR = BASE_DIR.joinpath('test_data')
--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -11,6 +11,14 @@ test_data_username_password = get_CSV_data_as_dict(filename)
 @pytest.mark.log_in
 @pytest.mark.parametrize("login, password", test_data_username_password)
 def test_login_with_valid_credentials(driver, login, password):
+    """
+    Preconditions:
+    The application is enabled on the start page - login page.
+    1.Insert login credentials
+    2.Insert password credentials
+    3.Click on login button
+    Expected result: Header is being displayed on main page of application
+    """
 
     login_page = UserInteractions(driver)
     login_page.login(login, password)
